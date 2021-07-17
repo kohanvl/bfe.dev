deca698f6ea20db3c461da2280cadec5670f6560
Vlad Kokhan <vvladd@users.noreply.github.com>
2021-07-17 19:06:00 +0300

    feature: add quiz answer

--- Quizzes/Block-scope.js
+++ Quizzes/Block-scope.js
@@ -6,4 +6,4 @@ for (var i = 0; i < 5; i++) {
 for (let i = 0; i < 5; i++) {
   setTimeout(() => console.log(i), 0);
 }
-// LET has block scope. FOR will log: 0 1 2 3 4
+// LET has block scope. FOR will log: 0 1 2 3 4 
