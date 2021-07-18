edce603c121298cf4675ca18d75cbfb80b43f897
Vlad Kokhan <vvladd@users.noreply.github.com>
2021-07-18 10:27:46 +0300

    feature: add quiz answer

---
+++ Quizzes/Equal.js
@@ -0,0 +1,8 @@
+console.log(0 == false)          // true - boolean converts to number false = 0 -> 0 == 0 = true
+console.log('' == false)         // true - boolean and string convert to number -> 0 == 0 = true
+console.log([] == false)         // true - object converts to primitive (to number) -> 0 == 0 = true
+console.log(undefined == false)  // false - undefined only == to undefined or null
+console.log(null == false)       // false - null only == to null or undefined
+console.log('1' == true)         // true - both convert to number -> 1 == 1 = true
+console.log(1n == true)          // true - same as above
+console.log(' 1     ' == true)   // true - same as above plus trim
