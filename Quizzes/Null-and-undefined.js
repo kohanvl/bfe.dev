8b4f79246ec52e0e3fc4717d1d6a2baa941d2453
Vlad Kokhan <vvladd@users.noreply.github.com>
2021-07-24 10:37:07 +0300

    feature: add quiz answer

---
+++ Quizzes/Null-and-undefined.js
@@ -0,0 +1,14 @@
+console.log(JSON.stringify([1,2,null,3]))       // "[1,2,null,3]"
+console.log(JSON.stringify([1,2,undefined,3]))  // "[1,2,null,3]" - JSON doesn't have undefined value
+console.log(null === undefined)                 // false - null == 0 and undefined == NaN
+console.log(null == undefined)                  // true - the rule: null olny == null/undefined
+console.log(null == 0)                          // false - the rule: null olny == null/undefined
+console.log(null < 0)                           // false - 0 < 0
+console.log(null > 0)                           // false - 0 > 0
+console.log(null <= 0)                          // true - 0 <= 0
+console.log(null >= 0)                          // true - 0 >= 0
+console.log(undefined == 0)                     // false - the rule: undefined olny == undefined/null
+console.log(undefined < 0)                      // false - NaN < 0
+console.log(undefined > 0)                      // false - NaN > 0
+console.log(undefined <= 0)                     // false - NaN <= 0
+console.log(undefined >= 0)                     // false - NaN >= 0
