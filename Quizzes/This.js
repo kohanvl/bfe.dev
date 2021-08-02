05fc0f5a1bb69e9181ba24abcb2f2962b49cbbec
Vlad Kokhan <vvlad0202@gmail.com>
2021-08-02 10:30:23 +0300

    feature: add quiz answer

---
+++ Quizzes/This.js
@@ -0,0 +1,39 @@
+const obj = {
+    a: 1,
+    b: function() {
+      console.log(this.a)
+    },
+    c() {
+      console.log(this.a)
+    },
+    d: () => {
+      console.log(this.a)
+    },
+    e: (function() {
+      return () => {
+        console.log(this.a);
+      }
+    })(),
+    f: function() {
+      return () => {
+        console.log(this.a);
+      }
+    }
+  }
+  
+  console.log(obj.a)  // 1
+  obj.b()             // 1
+  ;(obj.b)()          // 1
+  const b = obj.b
+  b()                 // undefined - reference was lost
+  obj.b.apply({a: 2}) // 2
+  obj.c()             // 1
+  obj.d()             // undefined - arrow function doesn't have own 'this'
+  ;(obj.d)()          // undefined - same as above
+  obj.d.apply({a:2})  // undefined - same as above
+  obj.e()             // undefined - same as above
+  ;(obj.e)()          // undefined - same as above
+  obj.e.call({a:2})   // undefined - same as above
+  obj.f()()           // 1 - because of closure 
+  ;(obj.f())()        // 1 - same as above
+  obj.f().call({a:2}) // 1 - closure already did lexical environment
