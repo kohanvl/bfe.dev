5d9ae5404be5ba064cc0cacff44ab63da9668e9b
Vlad Kokhan <vvlad0202@gmail.com>
2022-02-08 08:48:49 +0300

    feature: add 123. implement Promise.prototype.finally() solution

---
+++ Coding/Implement-Promise.prototype.finally().js
@@ -0,0 +1,12 @@
+/**
+ * Medium - https://bigfrontend.dev/problem/implement-Promise-prototype-finally
+ * @param {Promise<any>} promise
+ * @param {() => void} onFinally
+ * @returns {Promise<any>}
+ */
+function myFinally(promise, onFinally) {
+  return promise.then(
+    (value) => Promise.resolve(onFinally()).then(() => value),
+    (reason) => Promise.resolve(onFinally()).then(() => Promise.reject(reason)),
+  );
+}
