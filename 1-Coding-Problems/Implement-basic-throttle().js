cfdc272eede30626c0ee7d9e7372d3d4b92dd262
Vlad Kokhan <vvlad0202@gmail.com>
2022-10-28 10:28:40 +0400

    feature: add 60. create your own `new` operator solution

--- 1-Coding-Problems/Implement-basic-throttle().js
+++ 1-Coding-Problems/Implement-basic-throttle().js
@@ -2,6 +2,7 @@
  * @param {Function} func
  * @param {number} wait
  */
+// skip some funcs: ─A─B─C─ ─D─ ─ ─ ─ ─ ─ E─ ─F─G  ->  ─A─ ─ ─C─ ─ ─D ─ ─ ─ ─ E─ ─ ─G
 function throttle(func, wait) {
   let isThrottled = false;
   let lastAgs = null;
@@ -11,11 +12,11 @@ function throttle(func, wait) {
       lastAgs = args;
       return;
     }
-
+    // first input now
     func(...args);
 
     isThrottled = true;
-
+    // next input after wait ms
     setTimeout(() => {
       isThrottled = false;
       if (lastAgs) {
@@ -25,3 +26,22 @@ function throttle(func, wait) {
     }, wait);
   };
 }
+
+/*
+Используется для создания функции originalFn, которая будет вызываться не чаще чем delayMs. 
+/ Creates a throttled function that only invokes "originalFn" at most once per every "delayMs" milliseconds
+function throttle(originalFn, delayMs) {
+  let timeout; // timeout to keep track of the executions
+  return (...args) => {
+    if (timeout) { // if timeout is set, this is NOT the first execution, so ignore
+      return;
+    }
+    
+    / this is the first execution which we need to delay by "delayMs" milliseconds
+    timeout = setTimeout(() => {
+      originalFn(...args); // call the passed function with all arguments
+      timeout = null; // reset timeout so that the subsequent call launches the process anew
+    }, delayMs);
+  }
+}
+*/
